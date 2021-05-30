66c8a919d93b9e099c405e08ec2b7e30d5b55645
2102 <2104536632@qq.com>
2021-05-30 17:11:25 +0800

    注册 登录 时间

--- 5.29/reg.php
+++ 5.29/reg.php
@@ -1,10 +1,9 @@
 <?php
 
-    //接收 form 表单数据 $_POST
-    echo '<pre>';print_r($_POST);echo '</pre>';
+    //接收 form 表单数据 $_POST,是否符合规则
 
 
-
+echo '<pre>';print_r($_POST);echo '</pre>';
     //使用mysqli连接MYSQL
     $host = "127.0.0.1";  //mysql的主机地址
     $user = 'root';    //数据库的用户名
@@ -17,19 +16,26 @@
     $link = new mysqli($host,$user,$pass,$db);
 
     //处理变量
-    $username = trim($_POST['uname']);
-    $mobile = trim($_POST['mobile']);
-    $email = trim($_POST['email']);
+    $username = trim($_POST['u_name']);
+    $mobile = trim($_POST['u_mobile']);
+    $email = trim($_POST['u_email']);
+    $pass1 = trim($_POST['u_pass1']);
+    $pass2 = trim($_POST['u_pass2']);
+    $reg_time = time();       //注册时间
+    $age = intval($_POST['u_age']);   //年龄
+    $upass = password_hash($pass1,PASSWORD_BCRYPT);
 
-    //sql 语句
-    $sql = "insert into users (`username`,`mobile`,`email`) 
-    value ('{$username}','{$mobile}','{$email}')";
+
+
+//sql 语句
+    $sql = "insert into users (`username`,`mobile`,`email`,`pass`,`reg_time`,`age`) 
+    values ('{$username}','{$mobile}','{$email}','{$upass}','$reg_time','$age)";
 
     //准备阶段
     $stmt = mysqli_prepare($link,$sql);
 
     //执行阶段
-    $result = mysqli_execute($stmt);
+    $result = mysqli_stmt_execute($stmt);
 
     if($result){
         echo "insert 成功";
